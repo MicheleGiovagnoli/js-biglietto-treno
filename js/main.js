6f303e353ba4c95516efe39d6e4885163be9d330
michele <michele.giovagnoli1@gmail.com>
2023-02-22 17:11:56 +0100

    prezzo scontato per gli anziani, prezzo di base e nel caso venissero inseriti valori non idonei

--- js/main.js
+++ js/main.js
@@ -5,16 +5,14 @@
 //va applicato uno sconto del 40% per gli over 65.
 //L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+//Chideo all'utente i chilometri che vuole percorrere
 let chilometri = prompt('inserisci i chilometri che vuoi percorrere');
-console.log(chilometri);
-
+//Chiedo all'utente la sua eta
 let eta = prompt('inserisci i tuo anni');
-console.log(eta);
-
+//Calcolo il prezzo di base
 let prezzoBase = chilometri * 0.21;
-console.log(prezzoBase);
-
-let prezzoOver;
+//Calcolo i relattivi prezzi in base agli sconti del 20 per minori e del 40 per anziani
+let prezzoAnziani;
 let prezzoMinori;
 if( eta < 18 ) {
    prezzoMinori = ( ( prezzoBase * 20 ) / 100);
@@ -22,13 +20,16 @@ if( eta < 18 ) {
    prezzoMinori = prezzoMinori.toFixed(2);
    console.log(prezzoMinori);
    
-} else if ( eta > 65 ) {
-   prezzoOver = (( prezzoOver * 40 ) / 100);
-   
+} else if( eta > 65 ) {
+   prezzoAnziani = ( ( prezzoBase * 40 ) / 100);
+   prezzoAnziani = prezzoBase - prezzoAnziani;
+   prezzoAnziani = prezzoAnziani.toFixed(2);
+   console.log(prezzoAnziani);
+}
+ else if (eta > 18 || eta < 65) {
+   console.log(prezzoBase);
+}
 
-//} else if (eta > 18 || eta < 65) {
- //  console.log(prezzoBase);
-
-}else {
+else {
     console.log("Valore non accettato")
 }
